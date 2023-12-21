f7a66d0024f411464a33b5adf8670b891286da17
Sevan Babaxanloo <sevan_babaxanloo@yahoo.com>
2023-12-21 09:34:10 -0800

    done

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,21 +1,16 @@
-import React from 'react'
-import {auth} from '../firebase'
+import React from "react";
+import { auth } from "../firebase";
 
 const style = {
   button: `bg-[#395DFF] px-4 py-2 hover:bg-[#395DFF] rounded-lg text-white font-bold`,
 };
 
-
 const LogOut = () => {
-const signOut = () => {
-    signOut(auth)
-}
-
   return (
     <button onClick={() => auth.signOut()} className={style.button}>
-        Logout
+      Logout
     </button>
-  )
-}
+  );
+};
 
-export default LogOut
+export default LogOut;
